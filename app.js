44827e3b00c3e06685e399d51cf625e7717d0bef
Zsolt Kindla <goyslee@gmail.com>
2017-09-13 10:39:15 +0000

    added sender name

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.post("/contact/send", function(req, res){
         host: "smtp.gmail.com",
         port: 465,
         secure: true,
-        from: req.body.name + "<leafletcareforyourbusiness@gmail.com>",
+        from: "Leaflet Care <leafletcareforyourbusiness@gmail.com>",
         to: "leafletcareforyourbusiness@gmail.com",
         subject: "LeafletService",
         text: "You have a submission with the following details...  Quote?: " + req.body.quote + "Query?: " + req.body.query +  "Name: " + req.body.name + "Company/Organization: " + req.body.company + "Post Code: " + req.body.postcode + "Contact Number: " + req.body.contactNumber + "Email: " + req.body.email + "Message: " + req.body.message,
